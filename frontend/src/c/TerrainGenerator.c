c68fac8a922e558041234ff05ac75ffc2cff7d53
Wouter Deferme <wouter.deferme@gmail.com>
2015-04-02 00:01:11 +0200

    added things

--- frontend/src/c/TerrainGenerator.c
+++ frontend/src/c/TerrainGenerator.c
@@ -8,6 +8,27 @@
 
 #include <stdio.h>
 
+struct Vec3{
+    float x
+    float y
+    float z
+};
+
+struct PlaneVertices{
+    Vec3 topleft
+    Vec3 topright
+    Vec3 bottomleft
+    Vec3 bottomright
+};
+
+struct PlaneVertexNormals{
+    Vec3 normal
+};
+
+struct PlaneVertexIndices{
+    float index[6]
+};
+
 float[] generateVertexData(int chunkSize, float seed){
     return 0.0;
 }
